efabf1cc3fdc7478523bc6db9b8f7d7f4d997cfe
Aditya Chatterjee <aditianhacker@gmail.com>
2017-08-24 20:50:02 +0530

    import

--- sum.py
+++ sum.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python
 
 import argparse
+import os
+import re
+import subprocess
+import sys
+import math
+import pysrt
+import imageio
+imageio.plugins.ffmpeg.download()
+from moviepy.editor import *
+from itertools import starmap
+
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.nlp.stemmers import Stemmer
+from sumy.utils import get_stop_words
+from sumy.summarizers.lsa import LsaSummarizer
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("Watch videos quickly")
